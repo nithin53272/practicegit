2b9e62a3726a2d995b9531ec4b9b1d6855910057
Nithin Vishwanath <nvishwanath@Nithins-MacBook-Pro.local>
2018-08-06 10:31:45 +0530

    Initial commit

---
+++ whileloopgithub.rb
@@ -0,0 +1,15 @@
+puts("Enter the value for decrement")
+x = gets.chomp.to_i
+
+while x>=0
+	puts x
+	x = x-1
+end
+
+puts("Enter the value for increment")
+y= 0
+a = gets.chomp.to_i
+while y<=a
+	puts y
+	y = y+1
+end
